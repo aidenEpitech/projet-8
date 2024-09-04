8414a83397928c6479b6a4f0ee16b89c14e0b39c
Sofiane <dev@mihotel.fr>
2024-09-04 20:32:35 +0200

    config of rooter, adding pages, install of tailwind

---
+++ src/pages/Housing.jsx
@@ -0,0 +1,9 @@
+const Housing = () => {
+    return (
+        <div>
+            <h1>Housing Page</h1>
+        </div>
+    );
+};
+
+export default Housing;
---
+++ src/pages/components/layout/Footer.jsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <footer>
+            This is the footer
+        </footer>
+    );
+};
+
+export default Header;
